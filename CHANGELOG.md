4d4a06c307ca3ea130bb542b6752e901fb9ba29d
Joseph Scott <joseph@josephscott.org>
2018-11-07 22:34:52 -0700

    Add test.sh, which does a simple test for each supported resource type

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,4 @@
 ## Unreleased
 - Fix passing through binary items that are not minified
 - Bump html-minifier to 3.5.21
+- Added test.sh, a simple test for each supported resource type
---
+++ test.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+MD5="md5sum"
+if [ -e /sbin/md5 ]; then
+	MD5="/sbin/md5"
+fi
+
+# CSS
+CSS_MD5="42da7ab7cfa26de33b97038742d42a9b"
+CSS_TEST_MD5="$(curl -s 'http://localhost:4000/?url=https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.css' | $MD5)"
+
+if [ "$CSS_MD5" = "$CSS_TEST_MD5" ]; then
+	echo "CSS: Passed"
+else
+	echo "CSS: FAILED"
+fi
+
+# HTML
+HTML_MD5="d3c127c4d9e792aa498f9f57620cd230"
+HTML_TEST_MD5="$(curl -s 'http://localhost:4000/?url=https://wordpress.com/wp-content/themes/pub/vermilion-christmas/README.html' | $MD5)"
+
+if [ "$HTML_MD5" = "$HTML_TEST_MD5" ]; then
+	echo "HTML: Passed"
+else
+	echo "HTML: FAILED"
+fi
+
+# JS
+JS_MD5="f4914d3c444df19755fa37e2ec788f83"
+JS_TEST_MD5="$(curl -s 'http://localhost:4000/?url=https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js' | $MD5)"
+
+if [ "$JS_MD5" = "$JS_TEST_MD5" ]; then
+	echo "JavaScript: Passed"
+else
+	echo "JavaScript: FAILED"
+fi
+
+# SVG
+SVG_MD5="b90876922b20c6f46af491bbd18877d7"
+SVG_TEST_MD5="$(curl -s 'http://localhost:4000/?url=https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/svg2009.svg' | $MD5)"
+
+if [ "$SVG_MD5" = "$SVG_TEST_MD5" ]; then
+	echo "SVG: Passed"
+else
+	echo "SVG: FAILED"
+fi
