9eb605f56ef904be04a3ff2a594c3276c3fb95f0
Joseph Scott <joseph@josephscott.org>
2019-09-27 11:46:22 -0600

    Add compressed versions of tests for resources that we minify

--- test.sh
+++ test.sh
@@ -15,6 +15,15 @@ else
 	echo "CSS: FAILED"
 fi
 
+# CSS compressed
+CSS_COMPRESS_TEST_MD5="$(curl --compressed -s 'http://localhost:4000/?url=https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.css' | $MD5)"
+
+if [ "$CSS_MD5" = "$CSS_COMPRESS_TEST_MD5" ]; then
+	echo "CSS Compressed: Passed"
+else
+	echo "CSS Compressed: FAILED"
+fi
+
 # HTML
 HTML_MD5="d3c127c4d9e792aa498f9f57620cd230"
 HTML_TEST_MD5="$(curl -s 'http://localhost:4000/?url=https://wordpress.com/wp-content/themes/pub/vermilion-christmas/README.html' | $MD5)"
@@ -23,6 +32,15 @@ if [ "$HTML_MD5" = "$HTML_TEST_MD5" ]; then
 	echo "HTML: Passed"
 else
 	echo "HTML: FAILED"
+fi
+
+# HTML Compressed
+HTML_COMPRESS_TEST_MD5="$(curl --compressed -s 'http://localhost:4000/?url=https://wordpress.com/wp-content/themes/pub/vermilion-christmas/README.html' | $MD5)"
+
+if [ "$HTML_MD5" = "$HTML_COMPRESS_TEST_MD5" ]; then
+	echo "HTML Compressed: Passed"
+else
+	echo "HTML Compressed: FAILED"
 fi
 
 # JS
@@ -35,6 +53,15 @@ else
 	echo "JavaScript: FAILED"
 fi
 
+# JS Compressed
+JS_COMPRESS_TEST_MD5="$(curl --compressed -s 'http://localhost:4000/?url=https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js' | $MD5)"
+
+if [ "$JS_MD5" = "$JS_COMPRESS_TEST_MD5" ]; then
+	echo "JavaScript Compressed: Passed"
+else
+	echo "JavaScript Compressed: FAILED"
+fi
+
 # SVG
 SVG_MD5="3c6f0b9920a98c2cf05c8ae4bb29c073"
 SVG_TEST_MD5="$(curl -s 'http://localhost:4000/?url=https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/svg2009.svg' | $MD5)"
@@ -43,6 +70,15 @@ if [ "$SVG_MD5" = "$SVG_TEST_MD5" ]; then
 	echo "SVG: Passed"
 else
 	echo "SVG: FAILED"
+fi
+
+# SVG Compressed
+SVG_COMPRESS_TEST_MD5="$(curl --compressed -s 'http://localhost:4000/?url=https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/svg2009.svg' | $MD5)"
+
+if [ "$SVG_MD5" = "$SVG_COMPRESS_TEST_MD5" ]; then
+	echo "SVG Compressed: Passed"
+else
+	echo "SVG Compressed: FAILED"
 fi
 
 # Pass Through
