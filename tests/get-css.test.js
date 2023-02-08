394f07071321ce6afd7c444f058bcdad8213bce1
Joseph Scott <joseph@josephscott.org>
2023-02-08 16:16:20 -0700

    Set url in one place

--- tests/get-css.test.js
+++ tests/get-css.test.js
@@ -1,10 +1,11 @@
 "use strict";
 
 const supertest = require( 'supertest' );
+const target_url = 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.css';
 
 test( 'GET `/get` -- CSS', async () => {
 	const resp = await supertest( 'http://localhost:4747' )
-		.get( '/get?url=https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.css' )
+		.get( `/get?url=${target_url}` )
 		.expect( 200 )
 		.expect( 'Content-Type', /text\/css/ )
 		.expect( /Bootstrap v4.1.3/ )
@@ -13,7 +14,7 @@ test( 'GET `/get` -- CSS', async () => {
 
 test( 'GET `/get` -- CSS & gzip level 9', async () => {
 	const resp = await supertest( 'http://localhost:4747' )
-		.get( '/get?url=https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.css&with=gzip&level=9' )
+		.get( `/get?url=${target_url}&with=gzip&level=9` )
 		.expect( 200 )
 		.expect( 'Content-Type', /text\/css/ )
 		.expect( 'content-encoding', 'gzip' )
@@ -23,7 +24,7 @@ test( 'GET `/get` -- CSS & gzip level 9', async () => {
 
 test( 'GET `/get` -- CSS & br level 11', async () => {
 	const resp = await supertest( 'http://localhost:4747' )
-		.get( '/get?url=https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.css&with=br&level=11' )
+		.get( `/get?url=${target_url}&with=br&level=11` )
 		.expect( 200 )
 		.expect( 'Content-Type', /text\/css/ )
 		.expect( 'content-encoding', 'br' )
@@ -33,7 +34,7 @@ test( 'GET `/get` -- CSS & br level 11', async () => {
 
 test( 'GET `/get` -- CSS & deflate level 8', async () => {
 	const resp = await supertest( 'http://localhost:4747' )
-		.get( '/get?url=https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.css&with=deflate&level=8' )
+		.get( `/get?url=${target_url}&with=deflate&level=8` )
 		.expect( 200 )
 		.expect( 'Content-Type', /text\/css/ )
 		.expect( 'content-encoding', 'deflate' )
