2f13e867538b00224fd7b1df2f3fb38d47bd29e3
Joseph Scott <joseph@josephscott.org>
2023-02-08 16:16:08 -0700

    HTML tests

---
+++ tests/get-html.test.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const supertest = require( 'supertest' );
+const target_url = 'https://wordpress.com/wp-content/themes/pub/vermilion-christmas/README.html';
+
+test( 'GET `/get` -- HTML', async () => {
+	const resp = await supertest( 'http://localhost:4747' )
+		.get( `/get?url=${target_url}` )
+		.expect( 200 )
+		.expect( 'Content-Type', /text\/html/ )
+		.expect( /Vermillion Christmas 1.0/ )
+	;
+} );
+
+test( 'GET `/get` -- HTML & gzip level 9', async () => {
+	const resp = await supertest( 'http://localhost:4747' )
+		.get( `/get?url=${target_url}&with=gzip&level=9` )
+		.expect( 200 )
+		.expect( 'Content-Type', /text\/html/ )
+		.expect( 'content-encoding', 'gzip' )
+		.expect( 'x-compression-level', '9' )
+	;
+} );
+
+test( 'GET `/get` -- HTML & br level 11', async () => {
+	const resp = await supertest( 'http://localhost:4747' )
+		.get( `/get?url=${target_url}&with=br&level=11` )
+		.expect( 200 )
+		.expect( 'Content-Type', /text\/html/ )
+		.expect( 'content-encoding', 'br' )
+		.expect( 'x-compression-level', '11' )
+	;
+} );
+
+test( 'GET `/get` -- HTML & deflate level 8', async () => {
+	const resp = await supertest( 'http://localhost:4747' )
+		.get( `/get?url=${target_url}&with=deflate&level=8` )
+		.expect( 200 )
+		.expect( 'Content-Type', /text\/html/ )
+		.expect( 'content-encoding', 'deflate' )
+		.expect( 'x-compression-level', '8' )
+	;
+} );
