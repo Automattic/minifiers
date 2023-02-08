a03bf25da245ad5232d55f8c822846c47fe9a20e
Joseph Scott <joseph@josephscott.org>
2023-02-08 10:05:51 -0700

    Fix get CSS test

--- tests/get.test.js
+++ tests/get.test.js
@@ -4,9 +4,9 @@ const supertest = require( 'supertest' );
 
 test( 'GET `/get` -- CSS', async () => {
 	const resp = await supertest( 'http://localhost:4747' )
-		.get( '/get' )
+		.get( '/get?url=https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.css' )
 		.expect( 200 )
-		.expect('Content-Type', 'text/css')
-		.expect( { status: "OK" } )
+		.expect( 'Content-Type', /text\/css/ )
+		.expect( /Bootstrap v4.1.3/ )
 	;
 } );
