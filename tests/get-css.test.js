5e20fbc6d04e720b024abda201bee9faa0e45e2b
Joseph Scott <joseph@josephscott.org>
2023-05-08 14:32:26 -0600

    Add OPTIONS support for minified resources

--- tests/get-css.test.js
+++ tests/get-css.test.js
@@ -12,6 +12,15 @@ test( 'GET `/get` -- CSS', async () => {
 	;
 } );
 
+test( 'OPTIONS `/get` -- CSS', async () => {
+	const resp = await supertest( 'http://localhost:4747' )
+		.options( `/get?url=${target_url}` )
+		.expect( 200 )
+		.expect( 'Content-Type', /text\/css/ )
+		.expect( /Bootstrap v4.1.3/ )
+	;
+} );
+
 test( 'GET `/get` -- CSS & gzip level 9', async () => {
 	const resp = await supertest( 'http://localhost:4747' )
 		.get( `/get?url=${target_url}&with=gzip&level=9` )
