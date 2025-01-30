4cba364bf85e04d0f8911ec2a3e61d91168b35f2
Matthew Reishus <mreishus@users.noreply.github.com>
2025-01-30 16:57:53 +0000

    CSS Minification: Add test covering nesting :where(& > .bar)

--- tests/file-css.test.js
+++ tests/file-css.test.js
@@ -29,4 +29,31 @@ describe( 'file (css): Minify CSS files', () => {
 			) }% of original size`,
 		);
 	} );
+
+	test( 'GET `/file` -- CSS minify supports nesting :where(& > .bar)', async () => {
+		// This is from https://github.com/evanw/esbuild/issues/4005
+		const target_url = 'tests/files/nesting-test.css';
+		const originalContent = await fs.readFile( target_url, 'utf8' );
+
+		const resp = await request
+			.get( `/file?path=${ target_url }` )
+			.expect( 200 )
+			.expect( 'Content-Type', /text\/css/ )
+			.expect( 'x-minify', 't' );
+
+		const minifiedText = resp.text;
+		// Verify it was actually minified
+		const originalSize = originalContent.length;
+		const minifiedSize = minifiedText.length;
+		expect( minifiedSize ).toBeLessThan( originalSize * 0.85 );
+
+		// Verify the "purple" rule was not removed
+		expect( minifiedText ).toContain( 'purple' );
+
+		console.info(
+			`Minimized CSS ${ target_url } to ${ ( ( minifiedSize / originalSize ) * 100 ).toFixed(
+				2,
+			) }% of original size`,
+		);
+	} );
 } );
